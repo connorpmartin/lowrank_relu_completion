8e19817a3dabd01c03eb0d3946b0d7e0f14d9c0a
Connor Martin <connor825@gmail.com>
2021-06-07 11:20:26 -0400

    Update relu_completion_trend_testing.jl

---
+++ julia_testing/relu_completion_trend_testing.jl
@@ -0,0 +1,13 @@
+#sweep over row and column as well as underlying rank
+#find limits of rank and dimension experiments
+"""
+S. Ma, D. Goldfarb and L. Chen, "Fixed point and Bregman iterative methods for matrix rank minimization", 2009.
+K. Lee and Y. Bresler, "Admira: Atomic decomposition for minimum rank approximation", 2009.
+K. Toh and S. Yun, "An accelerated proximal gradient algorithm for nuclear norm regularized least squares problems", 2009.
+J.-F.Cai,E.J.Cande`s,andZ.Shen.Asingularvaluethresholding algorithm for matrix completion. SIAM Journal on Optimization, 20(4):1956–1982, 2008.
+"""
+#fields institute 
+#wed-fri: learning stuff!
+
+#generative model which makes those patterns high probability
+#maybe play around with models which specifically create relu-completable matrices?
