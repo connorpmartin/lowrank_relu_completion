2ab1391a0a252e356cacc8fd25e31f342ff1af56
Connor Martin <connor825@gmail.com>
2021-08-26 23:46:25 -0400

    Update sign_pattern_dist_checks.jl

--- julia_testing/sign_pattern_dist_checks.jl
+++ julia_testing/sign_pattern_dist_checks.jl
@@ -3,9 +3,26 @@ Checking for sign pattern distribution consistency!
 If the paper's statement is true, then regardless of the orientation of hyperplanes, the distribution of sign patterns should be exactly the same.
 
 """
-
 #monte carlo 
-
-    A = randn(40,10) * randn(10,40)
-    counta += any(.!(sum(A .> 0,dims=1) > 10))
-d
+rn = 100:25:300
+data = zeros(size(rn))
+global irtf = 1
+for n in rn
+    ybig,ycnt = n-25,0
+    for y in n-25:1:n
+        cnt = 0
+        for i in 1:200
+            if sum(sum(randn(n,y) * randn(y,n) .> 0,dims=1) .> 75) == n
+                cnt += 1
+            end
+        end
+        if cnt > ycnt
+            ycnt = cnt
+            ybig = y
+        end
+    end
+    data[irtf] = ybig
+    global irtf += 1
+end
+    plot(rn,data)
+    savefig("montecarlo.png")
